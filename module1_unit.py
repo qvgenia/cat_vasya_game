e10f2d11cc200fc43febd2411c15b5e36347732f
Evgen <evgen@gmail.com>
2026-05-05 20:10:09 +0300

    Модуль 3: добавлены заклинания и система маны

--- module1_unit.py
+++ module1_unit.py
@@ -8,6 +8,8 @@ class Unit(ABC):
         self.wisdom = wisdom
         self.intelligence = intelligence
         self.charisma = charisma
+        self.spells = []
+        self.mana = 0
 
     @abstractmethod
     def calculate_max_health(self):
@@ -20,3 +22,17 @@ class Unit(ABC):
     @abstractmethod
     def calculate_defense(self):
         pass
+
+    def add_spell(self, spell):
+        self.spells.append(spell)
+    
+    def cast_spell(self, index):
+        if index < 0 or index >= len(self.spells):
+            raise IndexError("Нет заклинания с таким индексом")
+        
+        spell = self.spells[index]
+        if self.mana >= spell.mana_cost:
+            self.mana -= spell.mana_cost
+            return spell.cast()
+        else:
+            raise Exception(f"Не хватает маны! Нужно {spell.mana_cost}, есть {self.mana}")
